13be685387c786e9c550802b8deba09f80249ff6
Axeldmont <114211438+Axeldmont@users.noreply.github.com>
2023-03-19 20:40:32 +0100

    Update Minesweeper.R

--- Démineur/Minesweeper.R
+++ Démineur/Minesweeper.R
@@ -1,4 +1,4 @@
-game <- function(ny,nx,prob){
+game <- function(nb_rows,nb_cols,prob){
   
   # Initialisation
   
@@ -10,27 +10,16 @@ game <- function(ny,nx,prob){
   
   # Création d'une fenètre de jeu :
   
-  sysName=Sys.info()['sysname']
-  {
-    if (toupper(substr(sysName,1,3)) == 'LIN' ){
-      dev.new(width=650/dpi, height=650/dpi/nx*(8+ny))
-    }
-    else if (toupper(substr(sysName,1,3)) == 'DAR'){
-      dev.new(width=650/dpi, height=650/dpi/nx*(8+ny))
-    }
-    else {
-      dev.new(width=650/dpi, height=650/dpi/nx*(8+ny))
-    }
-  }
+  dev.new(width = 650 / dpi, height = 650 / dpi / nb_cols * (8 + nb_rows))
   
   # Configuration de la zone de plot, titre et instruction : 
   
   par(mar=c(1,1,1,1))
-  plot(c(0,nx*w),c(0-5,(ny+2)*w),type='n',ann=FALSE , axes=FALSE)
+  plot(c(0,nb_cols*w),c(0-5,(nb_rows+2)*w),type='n',ann=FALSE , axes=FALSE)
   str_instruct='Clic gauche pour creuser.\nClic droit pour mettre un drapeau'
   text(0,-4,str_instruct,font=1,adj=c(0,0),cex=1.2)
-  text(nx/2,(ny +2),'Démineur',font=2, cex=2.5)
-  text(nx/2*0.995,(ny+2 )*0.995,'Démineur',font=2, cex=2.5,col='black')
+  text(nb_cols/2,(nb_rows +2),'Démineur',font=2, cex=2.5)
+  text(nb_cols/2*0.995,(nb_rows+2 )*0.995,'Démineur',font=2, cex=2.5,col='black')
   
   # Préparation des bombes :
   
@@ -49,49 +38,49 @@ game <- function(ny,nx,prob){
     
     if (col=='close'){
       polygon(c(i*w +r ,i*w +r,(i+1)*w -r,(i+1)*w -r ), 
-              c(ny*w-j*w - r,ny*w-(j+1)*w +r,ny*w-(j+1)*w +r ,ny*w-j*w -r),
+              c(nb_rows*w-j*w - r,nb_rows*w-(j+1)*w +r,nb_rows*w-(j+1)*w +r ,nb_rows*w-j*w -r),
               col='#BBBBBB',border=NA )    
       polygon(c(i*w ,i*w,i*w+r, i*w+r, (i+1)*w-r,(i+1)*w ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w, ny*w-(j+1)*w +r ,ny*w-j*w -r,ny*w-j*w -r,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w, nb_rows*w-(j+1)*w +r ,nb_rows*w-j*w -r,nb_rows*w-j*w -r,nb_rows*w-j*w ),
               col='#F8F8F8',border=NA ) 
       polygon(c(i*w ,(i+1)*w,(i+1)*w, (i+1)*w-r, (i+1)*w-r,i*w+r ), 
-              c(ny*w-(j+1)*w ,ny*w-(j+1)*w, ny*w-j*w  ,ny*w-j*w-r, ny*w-(j+1)*w+r,ny*w-(j+1)*w +r),
+              c(nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w, nb_rows*w-j*w  ,nb_rows*w-j*w-r, nb_rows*w-(j+1)*w+r,nb_rows*w-(j+1)*w +r),
               col='#999999',border=NA )   
     } else if (col=='open'){
       polygon(c(i*w  ,i*w ,(i+1)*w ,(i+1)*w  ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w ,ny*w-(j+1)*w  ,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w  ,nb_rows*w-j*w ),
               col='#BBBBBB',border='#777777' )    
     } else if (col=='bomb'){
       polygon(c(i*w  ,i*w ,(i+1)*w ,(i+1)*w  ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w ,ny*w-(j+1)*w  ,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w  ,nb_rows*w-j*w ),
               col='#BBBBBB',border='#777777' )    
-      polygon( ((i+1)-0.5)*w  + curIrclex * w/4  , (ny-(j+1)+0.5)*w  + curIrcley * w/4, col='black')
-      lines( c( ((i+1)-0.5)*w, ((i+1)-0.5)*w+w/3) , c( (ny-(j+1)+0.5)*w ,(ny-(j+1)+0.5)*w + w/3 ))
-      lines( c( ((i+1)-0.5)*w+w/3, ((i+1)-0.5)*w+w/3 +w/12  ) , c( (ny-(j+1)+0.5)*w + w/3,(ny-(j+1)+0.5)*w + w/3 -w/12 ) )
+      polygon( ((i+1)-0.5)*w  + curIrclex * w/4  , (nb_rows-(j+1)+0.5)*w  + curIrcley * w/4, col='black')
+      lines( c( ((i+1)-0.5)*w, ((i+1)-0.5)*w+w/3) , c( (nb_rows-(j+1)+0.5)*w ,(nb_rows-(j+1)+0.5)*w + w/3 ))
+      lines( c( ((i+1)-0.5)*w+w/3, ((i+1)-0.5)*w+w/3 +w/12  ) , c( (nb_rows-(j+1)+0.5)*w + w/3,(nb_rows-(j+1)+0.5)*w + w/3 -w/12 ) )
     } else if (col=='curbomb'){
       polygon(c(i*w  ,i*w ,(i+1)*w ,(i+1)*w  ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w ,ny*w-(j+1)*w  ,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w  ,nb_rows*w-j*w ),
               col='#FF0000',border='#777777' )   
-      polygon( ((i+1)-0.5)*w  + curIrclex * w/4  , (ny-(j+1)+0.5)*w  + curIrcley * w/4, col='black')
-      lines( c( ((i+1)-0.5)*w, ((i+1)-0.5)*w+w/3) , c( (ny-(j+1)+0.5)*w ,(ny-(j+1)+0.5)*w + w/3 ))
-      lines( c( ((i+1)-0.5)*w+w/3, ((i+1)-0.5)*w+w/3 +w/12 ) , c( (ny-(j+1)+0.5)*w + w/3,(ny-(j+1)+0.5)*w + w/3 -w/12 ) )
+      polygon( ((i+1)-0.5)*w  + curIrclex * w/4  , (nb_rows-(j+1)+0.5)*w  + curIrcley * w/4, col='black')
+      lines( c( ((i+1)-0.5)*w, ((i+1)-0.5)*w+w/3) , c( (nb_rows-(j+1)+0.5)*w ,(nb_rows-(j+1)+0.5)*w + w/3 ))
+      lines( c( ((i+1)-0.5)*w+w/3, ((i+1)-0.5)*w+w/3 +w/12 ) , c( (nb_rows-(j+1)+0.5)*w + w/3,(nb_rows-(j+1)+0.5)*w + w/3 -w/12 ) )
     } else if (col=='down'){
       polygon(c(i*w  ,i*w ,(i+1)*w ,(i+1)*w  ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w ,ny*w-(j+1)*w  ,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w  ,nb_rows*w-j*w ),
               col='#BBBBBB',border='#AAAAAA' )    
     } else if (col=='flag'){
       polygon(c(i*w +r ,i*w +r,(i+1)*w -r,(i+1)*w -r ), 
-              c(ny*w-j*w - r,ny*w-(j+1)*w +r,ny*w-(j+1)*w +r ,ny*w-j*w -r),
+              c(nb_rows*w-j*w - r,nb_rows*w-(j+1)*w +r,nb_rows*w-(j+1)*w +r ,nb_rows*w-j*w -r),
               col='#BBBBBB',border=NA )    
       polygon(c(i*w ,i*w,i*w+r, i*w+r, (i+1)*w-r,(i+1)*w ), 
-              c(ny*w-j*w ,ny*w-(j+1)*w, ny*w-(j+1)*w +r ,ny*w-j*w -r,ny*w-j*w -r,ny*w-j*w ),
+              c(nb_rows*w-j*w ,nb_rows*w-(j+1)*w, nb_rows*w-(j+1)*w +r ,nb_rows*w-j*w -r,nb_rows*w-j*w -r,nb_rows*w-j*w ),
               col='#F8F8F8',border=NA )   
       polygon(c(i*w ,(i+1)*w,(i+1)*w, (i+1)*w-r, (i+1)*w-r,i*w+r ), 
-              c(ny*w-(j+1)*w ,ny*w-(j+1)*w, ny*w-j*w  ,ny*w-j*w-r, ny*w-(j+1)*w+r,ny*w-(j+1)*w +r),
+              c(nb_rows*w-(j+1)*w ,nb_rows*w-(j+1)*w, nb_rows*w-j*w  ,nb_rows*w-j*w-r, nb_rows*w-(j+1)*w+r,nb_rows*w-(j+1)*w +r),
               col='#999999',border=NA )   
       
       xc=(i+0.5)*w - w/6;
-      yc=(ny-(j+1)+0.5)*w;
+      yc=(nb_rows-(j+1)+0.5)*w;
       a=w/2;
       b=w/9;
       c=w/4;
@@ -130,14 +119,14 @@ game <- function(ny,nx,prob){
   
   # Initialisations des variables :
   
-  img=matrix(as.numeric(runif(nx*ny) > prob ),nrow=ny);
+  img=matrix(as.numeric(runif(nb_cols*nb_rows) > prob ),nrow=nb_rows);
   num=img*0;
   
   # Comptage des bombes :
   
-  for (i in 1:ny){
-    for (j in 1:nx){ 
-      num[i,j]= sum( img[ max((i-1),1):min((i+1),ny), max((j-1),1):min((j+1),nx)]) 
+  for (i in 1:nb_rows){
+    for (j in 1:nb_cols){ 
+      num[i,j]= sum( img[ max((i-1),1):min((i+1),nb_rows), max((j-1),1):min((j+1),nb_cols)]) 
       }
   left=img*0;
   right=img*0;
@@ -146,8 +135,8 @@ game <- function(ny,nx,prob){
   taskFinished=0;
   }
   
-  for (i in 1:nx){
-    for (j in 1:ny){
+  for (i in 1:nb_cols){
+    for (j in 1:nb_rows){
       plotij(i,j, 'close')  
     }
   }
@@ -158,7 +147,7 @@ game <- function(ny,nx,prob){
   clickRestart=FALSE;
   clickQuit=FALSE
   bnum=paste('Bombes Restante :', formatC(sum(img)-sum(right),format='d',digits=3),'  '  ) ;
-  legend(nx/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
+  legend(nb_cols/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
   
   # Configuration Bouton : 
    
@@ -166,8 +155,8 @@ game <- function(ny,nx,prob){
   w2=2;
   r=0.3
   
-  plotbutton(nx*3/4,-1,w1,w2,r,'Quitter')
-  plotbutton(nx*2/4,-1,w1,w2,r,'Recommencer')
+  plotbutton(nb_cols*3/4,-1,w1,w2,r,'Quitter')
+  plotbutton(nb_cols*2/4,-1,w1,w2,r,'Recommencer')
   
   mousedown <-function(button,x,y){
     
@@ -175,8 +164,8 @@ game <- function(ny,nx,prob){
     y=grconvertY(y, "ndc", "user")
     cat("Buttons ", paste(button, collapse=" "), " at ", x, y, "\n")  
     
-    if ( !(x < (nx*w) & x > 0 & y < (ny*w) & y > 0 ) ){
-      x0=nx/2
+    if ( !(x < (nb_cols*w) & x > 0 & y < (nb_rows*w) & y > 0 ) ){
+      x0=nb_cols/2
       y0=-1
       w1=5;
       w2=2;
@@ -193,7 +182,7 @@ game <- function(ny,nx,prob){
                 col='#AA9999',border=NA )
         text(xloc+w1/2-0.01,yloc-w2/2-0.01,'Restart',font=1,cex=.7,col='#000000')
       }
-      x0=nx*3/4
+      x0=nb_cols*3/4
       y0=-1
       w1=5;
       w2=2;
@@ -215,7 +204,7 @@ game <- function(ny,nx,prob){
     
     if (taskFailure ==1 | taskFinished==1){return(NULL)}
     curI<<- ceiling(x/w);
-    curJ<<- ceiling((ny*w-y)/w);
+    curJ<<- ceiling((nb_rows*w-y)/w);
     curButton<<-button;
     
     if (left[curJ,curI] == 0 & button[1] !=1){ 
@@ -238,13 +227,13 @@ game <- function(ny,nx,prob){
       
       # Initialisation des variables :
       
-      img<<-matrix(as.numeric(runif(nx*ny) > prob ),nrow=ny);
+      img<<-matrix(as.numeric(runif(nb_cols*nb_rows) > prob ),nrow=nb_rows);
       num<<-img*0;
       
       # Comptage des Bombes : 
       
-      for (i in 1:ny){
-        for (j in 1:nx){ num[i,j]<<- sum( img[ max((i-1),1):min((i+1),ny)      ,  max((j-1),1):min((j+1),nx)     ]) 
+      for (i in 1:nb_rows){
+        for (j in 1:nb_cols){ num[i,j]<<- sum( img[ max((i-1),1):min((i+1),nb_rows)      ,  max((j-1),1):min((j+1),nb_cols)     ]) 
         }
       }
       
@@ -254,8 +243,8 @@ game <- function(ny,nx,prob){
       taskFailure<<-0;
       taskFinished<<-0;
       
-      for (i in 1:nx){
-        for (j in 1:ny){
+      for (i in 1:nb_cols){
+        for (j in 1:nb_rows){
           plotij(i,j, 'close' )  
         }
       }
@@ -267,24 +256,24 @@ game <- function(ny,nx,prob){
       w1=5;
       w2=2;
       r=0.3
-      plotbutton(nx*3/4,-1,w1,w2,r,'Quitter')
-      plotbutton(nx*2/4,-1,w1,w2,r,'Recommencer')
+      plotbutton(nb_cols*3/4,-1,w1,w2,r,'Quitter')
+      plotbutton(nb_cols*2/4,-1,w1,w2,r,'Recommencer')
       
       bnum=paste('Bombes Restante :', formatC(sum(img)-sum(right),format='d',digits=3),'  '  ) ;
-      legend(nx/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
+      legend(nb_cols/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
       
       return(NULL)
     }
     if (clickQuit){
       dev.off()
     }
-    if ( !(x < (nx*w) & x > 0 & y < (ny*w) & y > 0 )){      
+    if ( !(x < (nb_cols*w) & x > 0 & y < (nb_rows*w) & y > 0 )){      
       return(NULL)
     }
     if (taskFailure ==1 | taskFinished==1){return(NULL)}
     
     i= ceiling(x/w);
-    j= ceiling((ny*w-y)/w);
+    j= ceiling((nb_rows*w-y)/w);
     
     if (i!=curI |j !=curJ ){
       cat('bbbb\n')
@@ -324,8 +313,7 @@ game <- function(ny,nx,prob){
           }
           plotij(i,j,'curbomb')
           taskFailure<<-1;
-          text(nx/2,ny/2.,'Game over!',font=2,cex=3.5)
-          text(nx/2*0.99,ny/2*0.99,'Game over !',font=2,cex=3.5,col='#FF0000')
+          text(nb_cols/2,nb_rows/2.,'Game over!',font=2,cex=3.5,col='red')
         }
       }
     }
@@ -336,17 +324,16 @@ game <- function(ny,nx,prob){
           right[j,i]<<-1;
           if( all(img==right)){
             taskFinished<<-1;
-            text(nx/2,ny/2.,'Victoire !',font=2,cex=3.5)
-            text(nx/2*0.99,ny/2*0.99,'Victoire !',font=2,cex=3.5,col='#FF0000')
+            text(nb_cols/2,nb_rows/2.,'Victoire !',font=2,cex=3.5, col='green')
           }
           bnum=paste('Bombes Restante :', formatC(sum(img)-sum(right),format='d',digits=3),'  '  ) ;
-          legend(nx/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
+          legend(nb_cols/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")   
         }
         else{
           plotij(i,j,'close')
           right[j,i]<<-0;
           bnum=paste('Bombes Restante :', formatC(sum(img)-sum(right),format='d',digits=3),'  '  ) ;
-          legend(nx/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")  
+          legend(nb_cols/2, -3.5,bnum,cex=1.2,text.col="blue", box.col="red",bg="yellow")  
         }
       }
     }
@@ -363,7 +350,7 @@ game <- function(ny,nx,prob){
       idx=num[j,i];
       idx=min(3,idx);
       col=c('blue','green','red')
-      text( (i-0.5)*w ,(ny-j+0.5)*w, toString(num[j,i]),col=col[idx], font=2 )
+      text( (i-0.5)*w ,(nb_rows-j+0.5)*w, toString(num[j,i]),col=col[idx], font=2 )
     }
   }
 
@@ -373,7 +360,7 @@ game <- function(ny,nx,prob){
         curJ=(j+M);
         curI=(i+N);
         if (M ==0 & N==0){next}
-        if (curJ < 1 | curJ > ny | curI <1 | curI > nx){next}
+        if (curJ < 1 | curJ > nb_rows | curI <1 | curI > nb_cols){next}
         if (right[curJ,curI] ==0){
           plotij(curI,curJ,'open') ; plottext(curI,curJ)       
           left[curJ,curI]<<-1;       
@@ -386,8 +373,8 @@ game <- function(ny,nx,prob){
   }
   
   hitflag <- function (i,j){    
-    imgsub=img[ max(j-1,1):min(j+1,ny) ,  max(i-1,1):min(i+1,nx)        ]
-    rightsub=right[ max(j-1,1):min(j+1,ny) ,  max(i-1,1):min(i+1,nx)        ]
+    imgsub=img[ max(j-1,1):min(j+1,nb_rows) ,  max(i-1,1):min(i+1,nb_cols)        ]
+    rightsub=right[ max(j-1,1):min(j+1,nb_rows) ,  max(i-1,1):min(i+1,nb_cols)        ]
     if (all(imgsub==rightsub) & max(imgsub) == 1)
     {
       for(M in -1:1){
@@ -395,7 +382,7 @@ game <- function(ny,nx,prob){
           curJ=(j+M);
           curI=(i+N);
           if (M ==0 & N==0) {next}
-          if (curJ < 1 | curJ > ny | curI <1 | curI > nx) {next}
+          if (curJ < 1 | curJ > nb_rows | curI <1 | curI > nb_cols) {next}
           if (right[curJ,curI] == 0){
             plotij(curI,curJ,'open') ; plottext(curI,curJ)       
             left[curJ,curI]<<-1;       
@@ -411,17 +398,19 @@ game <- function(ny,nx,prob){
 
 #########################################################################################################
 
+# Gestion de la difficulté : 
+
 Minesweeper <- function(n){
   if (n == 5) {
-    mygame(10,20,0.01) # Luck
+    game(10,20,0.01) # Luck
   } else if (n == 4){
-    mygame(10,20,0.7) # Expert
+    game(10,20,0.7) # Expert
   } else if (n == 3){
-    mygame(10,20,0.8)  # hard
+    game(10,20,0.8)  # hard
   } else if (n == 2){
-    mygame(10,20,0.9) # Medium
+    game(10,20,0.9) # Medium
   } else {
-    mygame(10,20,0.95) # Easy
+    game(10,20,0.95) # Easy
   }
 }
-Minesweeper(3)
+Minesweeper(1)
